4bf770559e54c3f816a61cd6a0bdda8a7e189c9b
Vlad <murdoc-94@mail.ru>
2022-12-17 16:58:58 +0300

    lesson_7 cache_location fixed

--- config/settings.py
+++ config/settings.py
@@ -200,7 +200,7 @@ LOGGING = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://192.168.0.20:6379",
+        "LOCATION": "redis://127.0.0.1:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
